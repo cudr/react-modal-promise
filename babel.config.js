d1bef393e0052272aca7fb622030242c9cff346e
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2019-04-11 11:13:55 +0200

    Applied changes proposed by @cudr

--- babel.config.js
+++ babel.config.js
@@ -17,13 +17,7 @@ module.exports = function(api) {
       "@babel/react",
     ],
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-transform-react-jsx",
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      "@babel/plugin-proposal-function-sent",
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-numeric-separator",
-      "@babel/plugin-proposal-throw-expressions",
+      "@babel/plugin-proposal-class-properties"
     ],
   };
 };
