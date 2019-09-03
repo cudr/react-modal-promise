c7204d713d9fb587d071894319351de8105f1197
cudr <codefaster@yandex.ru>
2019-09-03 12:10:32 +0300

    feat: update prettier config

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ yarn add react-modal-promise
 
 1. Place ModalContainer in root of your App:
 
-```
+```tsx
 import ModalContainer from 'react-modal-promise'
 
 class MyApp extends React.Component {
@@ -25,15 +25,15 @@ class MyApp extends React.Component {
 
 2. Create you own modal component:
 
-(You need pass ```open: bool``` flag to you Modal component)
+(You need pass ```open: boolean``` flag to you Modal component)
 
 Possible to close modal and resolve Promise using close() function from props
 
-```
+```tsx
 import { createModal } from 'react-modal-promise'
 import { Modal } from 'react-bootstrap'
 
-const MyModal = ({ open: bool, close: function, /*...any props was passed to modal*/ }) => (
+const MyModal = ({ open: boolean, close: (params: any /*...any params passed to modal*/) }) => (
   <Modal open={open} onHide={() => close()}>
     My Super Promised modal
     <button onClick={() => close(/*pass any value*/)}>Confirm modal</button>
@@ -43,14 +43,14 @@ const MyModal = ({ open: bool, close: function, /*...any props was passed to mod
 
 And wrap it:
 
-```
+```tsx
 const myPromiseModal = createModal(MyModal)
 
 ```
 
 3. Use modal as Promise everywhere:
 
-```
+```tsx
 myPromiseModal({ /*pass any props here*/ }).then(value => {
   // get value that you pass to 'close' function
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,74 +1,74 @@
-import React from "react";
-import ModalFactory from "./Factory";
+import React from 'react'
+import ModalFactory from './Factory'
 
-export type Scope = string;
+export type Scope = string
 
 type InjectedModalProps<Result> = {
-  open: boolean;
-  close: (result: Result) => void;
-};
+  open: boolean
+  close: (result: Result) => void
+}
 
 export type ModalOptions = {
-  scope?: Scope;
-  exitTimeout?: number;
-  enterTimeout?: number;
-};
+  scope?: Scope
+  exitTimeout?: number
+  enterTimeout?: number
+}
 
 export type ControllerPropsModel = {
-  scope?: Scope;
-};
+  scope?: Scope
+}
 
 export interface CreateModalModel {
   <T extends InjectedModalProps<Result>, Result = boolean>(
     Component: React.ComponentType<T>,
     options?: ModalOptions
-  ): (props?: Omit<T, "open" | "close">) => Promise<Result>;
+  ): (props?: Omit<T, 'open' | 'close'>) => Promise<Result>
 }
 
 interface FactoryStackModel {
-  [key: string]: any;
+  [key: string]: any
 }
 
 declare global {
   interface Window {
-    factoryStack: FactoryStackModel;
+    factoryStack: FactoryStackModel
   }
 }
 
-const defaultScope: Scope = "stack";
+const defaultScope: Scope = 'stack'
 
-let factoryStack: FactoryStackModel = {};
+let factoryStack: FactoryStackModel = {}
 
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   if (!window.factoryStack) {
-    window.factoryStack = {};
+    window.factoryStack = {}
   }
 
-  factoryStack = window.factoryStack;
+  factoryStack = window.factoryStack
 }
 
-const PromiseController = ({
+const PromiseController: React.FC<ControllerPropsModel> = ({
   scope = defaultScope,
   ...rest
-}: ControllerPropsModel) => (
+}) => (
   <ModalFactory
     {...rest}
     ref={(node: ModalFactory) => {
-      factoryStack[scope] = node;
+      factoryStack[scope] = node
     }}
   />
-);
+)
 
 PromiseController.defaultProps = {
   scope: defaultScope
-};
+}
 
 const createModal: CreateModalModel = (Component, options) => props =>
   factoryStack[(options && options.scope) || defaultScope].create(
     Component,
     options
-  )(props);
+  )(props)
 
-export default PromiseController;
+export default PromiseController
 
-export { createModal };
+export { createModal }
